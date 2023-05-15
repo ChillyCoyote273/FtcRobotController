37485a4c4636d0b9b25733cb54b89b1f6e6abba1
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2023-05-15 14:07:10 -0400

    Got Rust working with JNI

---
+++ rust/src/lib.rs
@@ -0,0 +1,13 @@
+use jni::JNIEnv;
+use jni::objects::JClass;
+use jni::sys::jint;
+
+#[no_mangle]
+pub extern "system" fn Java_org_firstinspires_ftc_teamcode_RustTest_multiply<'local>(
+    mut env: JNIEnv<'local>,
+    class: JClass<'local>,
+    a: jint,
+    b: jint
+) -> jint {
+    a * b
+}
